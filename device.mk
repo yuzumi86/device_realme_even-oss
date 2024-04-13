364122364d85248d9ac87eb8b8034e37ae63ede0
eun0115 <gianpaoloestacio5@gmail.com>
2024-04-13 19:31:19 +0800

    even: Drop RealmeDirac
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- device.mk
+++ device.mk
@@ -28,9 +28,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # IMS
 $(call inherit-product, vendor/realme/even-ims/even-ims.mk)
 
-# RealmeParts
-$(call inherit-product, packages/apps/RealmeParts/parts.mk)
-
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
