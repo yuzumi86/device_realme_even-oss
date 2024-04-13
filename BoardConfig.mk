fddecd0c9d35393d309ac8fd51cb9f890fc1d125
Gian Paolo <gianpaoloestacio5@gmail.com>
2024-04-13 00:25:48 +0000

    even: Use the correct cpu variant
    Signed-off-by: Gian Paolo <gianpaoloestacio5@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a75
+TARGET_2ND_CPU_VARIANT := cortex-a55
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
