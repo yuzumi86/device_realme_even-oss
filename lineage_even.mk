23a5c98aeb0218d3e782a50086735a41469270e4
eun0115 <gianpaoloestacio5@gmail.com>
2024-04-22 22:03:34 +0800

    even: Switch to lineage
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- lineage_even.mk
+++ lineage_even.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit some common Pixel-Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/even/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_even
+PRODUCT_NAME := lineage_even
 PRODUCT_DEVICE := even
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme C25 Series
