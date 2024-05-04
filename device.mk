3bcd5070b0f5beec3aba98a42b75f105cf9fd816
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-05-04 08:08:46 +0700

    even: fixed a typing error

--- device.mk
+++ device.mk
@@ -93,7 +93,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml
 
- Health
+ # Health
  PRODUCT_PACKAGES += \
      android.hardware.health-service.even \
      android.hardware.health-service.even-recovery
